94a24c46432232b134816b7e3bf4c27be16f3c8e
arditnimanaj1999 <arditnimanaj@gmail.com>
2024-04-24 20:47:34 +0200

    small bugs fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import hero from "../resources/download.png";
+import { Typewriter } from "react-simple-typewriter";
 
 function Hero() {
   return (
@@ -14,7 +15,7 @@ function Hero() {
       <div className="absolute inset-0 bg-gray-900 opacity-50"></div>{" "}
       {/* Add a semi-transparent gray overlay */}
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white py-20 md:px-32 border w-fit h-fit mx-auto my-auto bg-rose-900 opacity-60">
-        <h1 className="text-5xl font-bold max-w-md">
+        <h1 className="text-2xl md:text-5xl font-bold max-w-md">
           WELCOME TO <br></br>BINI AUTOREPAIR INC
         </h1>
         <p className="text-center pt-2 font-bold max-w-sm md:max-w-lg">
