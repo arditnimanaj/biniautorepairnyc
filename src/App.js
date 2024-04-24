8b1a034891c4642498571bfb76bebce2ba25fa17
arditnimanaj1999 <arditnimanaj@gmail.com>
2024-04-24 21:51:16 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx";
 import Footer from "./components/Footer.jsx";
-import Services from "./components/Services.jsx";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import ServicesPage from "./pages/ServicesPage.jsx";
